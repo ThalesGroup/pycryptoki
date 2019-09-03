b50493d7cd9c803ad402bfeac9874068c4c3cbf1
Ashley Straw <ashley.straw@gemalto.com>
2019-09-03 11:32:50 -0400

    LA-3548 Adding more optional args
    Change-Id: I55f09027da2875cec58cfe622537acadf2c74285

--- pycryptoki/mechanism/rsa.py
+++ pycryptoki/mechanism/rsa.py
@@ -5,8 +5,7 @@ from ctypes import c_void_p, cast, pointer, sizeof
 
 from .helpers import Mechanism
 from ..attributes import to_byte_array
-from ..cryptoki import CK_ULONG, CK_RSA_PKCS_OAEP_PARAMS, \
-    CK_RSA_PKCS_PSS_PARAMS
+from ..cryptoki import CK_ULONG, CK_RSA_PKCS_OAEP_PARAMS, CK_RSA_PKCS_PSS_PARAMS
 from ..defines import *
 
 
@@ -15,7 +14,9 @@ class RSAPKCSOAEPMechanism(Mechanism):
     Create the required RSA_PKCS_OAEP param structure & convert python data to
     C data.
     """
-    REQUIRED_PARAMS = ['hashAlg', 'mgf']
+
+    REQUIRED_PARAMS = ["hashAlg", "mgf"]
+    OPTIONAL_PARAMS = ["sourceData"]
 
     def to_c_mech(self):
         """
@@ -25,15 +26,15 @@ class RSAPKCSOAEPMechanism(Mechanism):
         """
         super(RSAPKCSOAEPMechanism, self).to_c_mech()
         oaep_params = CK_RSA_PKCS_OAEP_PARAMS()
-        oaep_params.hashAlg = CK_ULONG(self.params['hashAlg'])
-        oaep_params.mgf = CK_ULONG(self.params['mgf'])
+        oaep_params.hashAlg = CK_ULONG(self.params["hashAlg"])
+        oaep_params.mgf = CK_ULONG(self.params["mgf"])
         # Note: According to
         # https://www.cryptsoft.com/pkcs11doc/v220
         # /group__SEC__12__1__7__PKCS____1__RSA__OAEP__MECHANISM__PARAMETERS.html
         # there is only one encoding parameter source.
         oaep_params.source = CK_ULONG(CKZ_DATA_SPECIFIED)
-        if 'sourceData' in self.params:
-            data, data_len = to_byte_array(self.params['sourceData'])
+        if "sourceData" in self.params:
+            data, data_len = to_byte_array(self.params["sourceData"])
         else:
             data = None
             data_len = 0
@@ -50,7 +51,9 @@ class RSAPKCSPSSMechanism(Mechanism):
     Create the required RSA_PKCS_PSS param structure & convert python data to
     C data.
     """
-    REQUIRED_PARAMS = ['hashAlg', 'mgf']
+
+    REQUIRED_PARAMS = ["hashAlg", "mgf"]
+    OPTIONAL_PARAMS = ["usSaltLen"]
 
     def to_c_mech(self):
         """
@@ -61,9 +64,9 @@ class RSAPKCSPSSMechanism(Mechanism):
         """
         super(RSAPKCSPSSMechanism, self).to_c_mech()
         c_params = CK_RSA_PKCS_PSS_PARAMS()
-        c_params.hashAlg = CK_ULONG(self.params['hashAlg'])
-        c_params.mgf = CK_ULONG(self.params['mgf'])
-        c_params.usSaltLen = CK_ULONG(self.params.get('usSaltLen', 8))
+        c_params.hashAlg = CK_ULONG(self.params["hashAlg"])
+        c_params.mgf = CK_ULONG(self.params["mgf"])
+        c_params.usSaltLen = CK_ULONG(self.params.get("usSaltLen", 8))
         self.mech.pParameter = cast(pointer(c_params), c_void_p)
         self.mech.usParameterLen = CK_ULONG(sizeof(c_params))
         return self.mech
