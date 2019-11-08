11b2a2a65a6196699086bdeb5a55223e38dd1750
10054225 <hamidreza.ghafghazi@thalesgroup.com>
2019-11-08 13:51:19 -0500

    SH-4613 setting sync_request_timeout
    Change-Id: I68ea7f24c8f52f1f92e7613955d8e8ae05626568

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     description="A python wrapper around the C cryptoki library.",
     author="Ashley Straw",
     url="https://github.com/gemalto/pycryptoki",
-    version="2.5.5",
+    version="2.5.6",
     packages=[
         "pycryptoki",
         "pycryptoki.cryptoki",
