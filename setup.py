6731320f95aa9bc1722a56d26d56a38758408716
Ashley C Straw <as.fireflash38@gmail.com>
2020-06-08 10:39:52 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     description="A python wrapper around the C cryptoki library.",
     author="Ashley Straw",
     url="https://github.com/gemalto/pycryptoki",
-    version="2.5.11",
+    version="2.5.12",
     packages=[
         "pycryptoki",
         "pycryptoki.cryptoki",
